99c45ed7e9943dde6e48ac0547d87c25a28c7fed
tanirocr@gmail.com <alakazamxdgithub12>
2017-08-08 21:57:09 -0300

    Initial commit

---
+++ app/src/main/java/br/ufrn/eaj/tads/exemploframe/MainActivity.java
@@ -0,0 +1,21 @@
+package br.ufrn.eaj.tads.exemploframe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickbotao (View v){
+        View barra = findViewById(R.id.barra);
+        barra.setVisibility(View.VISIBLE);
+
+        v.setVisibility(View.INVISIBLE);
+    }
+}
