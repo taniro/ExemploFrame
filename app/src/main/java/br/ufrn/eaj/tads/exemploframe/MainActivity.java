1cedc9b7320c48acc009800bbce8e633be452049
tanirocr@gmail.com <alakazamxdgithub12>
2018-08-14 21:26:29 -0300

    updates para 2018

--- app/src/main/java/br/ufrn/eaj/tads/exemploframe/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/exemploframe/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
